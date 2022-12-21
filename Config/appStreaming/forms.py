dc18947b748072d183d0e3186b7dde930b64f233
Canepa225 <canepa.igna@gmail.com>
2022-12-21 10:11:31 -0300

    form added date in html

--- Config/appStreaming/forms.py
+++ Config/appStreaming/forms.py
@@ -4,12 +4,25 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Productor
 
 
+class DateTimeInput(forms.DateTimeInput):
+	input_type = 'date'
+
+
 
 class NuevoStreaming(forms.ModelForm):
     class Meta:
         model = Evento 
         fields = '__all__'
         
+        widgets = {
+            'inicioStream' : DateTimeInput(),
+            'finStream' : DateTimeInput(),
+            'fechaConfirmacion' : DateTimeInput(),
+            'fechaRegistro' : DateTimeInput(),
+        }
+        
+        
+
 class NuevoProductor(forms.ModelForm):
     class Meta:
         model = Productor 
